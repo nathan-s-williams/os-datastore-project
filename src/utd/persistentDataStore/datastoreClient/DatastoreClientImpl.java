3c1f015ef1162b840f7ab3df950a604475e31188
Nathan Williams <nate88williams@gmail.com>
2022-11-13 17:39:02 -0600

    First successful implementation. All tests passed. Still need to handle exceptions correctly in read and directory classes.

--- src/utd/persistentDataStore/datastoreClient/DatastoreClientImpl.java
+++ src/utd/persistentDataStore/datastoreClient/DatastoreClientImpl.java
@@ -11,8 +11,17 @@
  
 package utd.persistentDataStore.datastoreClient;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.ArrayList;
 import java.util.List;
+
+import utd.persistentDataStore.utils.StreamUtil;
 
 public class DatastoreClientImpl implements DatastoreClient
 {
@@ -31,8 +40,38 @@ public class DatastoreClientImpl implements DatastoreClient
 	@Override
     public void write(String name, byte data[]) throws ClientException, ConnectionException
 	{
-		// Replace with implementation
-		throw new RuntimeException("Executing Write Operation");
+		try {
+			//System.out.println("Opening Socket");
+			Socket socket = new Socket();
+			SocketAddress saddr = new InetSocketAddress(address, port);
+			socket.connect(saddr);
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			
+			
+			StreamUtil.writeLine("write\n", outputStream);
+			StreamUtil.writeLine(name, outputStream);
+			StreamUtil.writeLine(data.length + "\n", outputStream);
+			StreamUtil.writeData(data, outputStream);
+			
+			String response = StreamUtil.readLine(inputStream);
+			if(response.contentEquals("OK"))
+			{
+				System.out.println(response);
+				outputStream.close();
+				inputStream.close();
+				socket.close();
+			}
+			else
+			{
+				socket.close();
+				throw new IOException(response);
+			}
+			
+		}
+		catch (IOException ex) {
+			throw new ClientException(ex.getMessage(), ex);
+		}
 	}
 
 	/* (non-Javadoc)
@@ -41,8 +80,55 @@ public class DatastoreClientImpl implements DatastoreClient
 	@Override
     public byte[] read(String name) throws ClientException, ConnectionException
 	{
-		// Replace with implementation
-		throw new RuntimeException("Executing Read Operation");
+		try {
+			//System.out.println("Opening Socket");
+			Socket socket = new Socket();
+			SocketAddress saddr = new InetSocketAddress(address, port);
+			socket.connect(saddr);
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			
+			//System.out.println("Writing Message");
+			StreamUtil.writeLine("read\n", outputStream);
+			StreamUtil.writeLine(name, outputStream);
+			
+			String response = StreamUtil.readLine(inputStream);
+			if (!"ok".equalsIgnoreCase(response.trim())) 
+			{
+				socket.close();
+				
+				throw new ClientException(response);				
+			}
+			else 
+			{
+				int size = Integer.parseInt(StreamUtil.readLine(inputStream));
+				
+				byte[] data = new byte[size];
+				
+				data = StreamUtil.readData(size, inputStream);
+				
+				System.out.print(response + "\n");
+				System.out.print(size + "\n");
+
+				for(int runner = 0; runner < size; runner++)
+				{
+					System.out.print(data[runner] + " ");
+				}
+				System.out.print("\n");
+				
+				outputStream.close();
+				inputStream.close();
+				socket.close();
+				
+				return data;
+				
+			}
+			
+			
+		}
+		catch (IOException ex) {
+			throw new ClientException(ex.getMessage(), ex);
+		}
 	}
 
 	/* (non-Javadoc)
@@ -51,9 +137,41 @@ public class DatastoreClientImpl implements DatastoreClient
 	@Override
     public void delete(String name) throws ClientException, ConnectionException
 	{
-		// Replace with implementation
-		throw new RuntimeException("Executing Delete Operation");
+		try {
+			Socket socket = new Socket();
+			SocketAddress saddr = new InetSocketAddress(address, port);
+			socket.connect(saddr);
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			
+			
+			StreamUtil.writeLine("delete\n", outputStream);
+			StreamUtil.writeLine(name, outputStream);
+			
+			String response = StreamUtil.readLine(inputStream);
+			if(response.contentEquals("OK"))
+			{
+				System.out.println(response);
+				outputStream.close();
+				inputStream.close();
+				socket.close();
+			}
+			else
+			{
+				socket.close();
+				throw new IOException(response);
+			}
+				
+			
+			
+			
+		}
+		catch (IOException ex) {
+			throw new ClientException(ex.getMessage(), ex);
+		}
 	}
+	
+	
 
 	/* (non-Javadoc)
 	 * @see utd.persistentDataStore.datastoreClient.DatastoreClient#directory()
@@ -61,8 +179,52 @@ public class DatastoreClientImpl implements DatastoreClient
 	@Override
     public List<String> directory() throws ClientException, ConnectionException
 	{
-		// Replace with implementation
-		throw new RuntimeException("Executing Directory Operation");
+		try {
+			Socket socket = new Socket();
+			SocketAddress saddr = new InetSocketAddress(address, port);
+			socket.connect(saddr);
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			
+			
+			StreamUtil.writeLine("directory\n", outputStream);
+			
+			
+			String response = StreamUtil.readLine(inputStream);
+			if(response.contentEquals("OK"))
+			{
+				System.out.print(response + "\n");
+				int size = Integer.parseInt(StreamUtil.readLine(inputStream));
+				System.out.print(size + "\n");
+				
+				List<String> names = new ArrayList<String>();
+				
+				for(int runner = 0; runner < size; runner++)
+				{
+					names.add(StreamUtil.readLine(inputStream));
+					System.out.print(names.get(runner) + "\n");
+				}
+				
+				
+				outputStream.close();
+				inputStream.close();
+				socket.close();
+				
+				return names;
+			}
+			else
+			{
+				socket.close();
+				throw new IOException(response);
+			}
+				
+			
+			
+			
+		}
+		catch (IOException ex) {
+			throw new ClientException(ex.getMessage(), ex);
+		}
 	}
 
 }
--- src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
+++ src/utd/persistentDataStore/datastoreServer/commands/ReadCommand.java
@@ -3,12 +3,32 @@ package utd.persistentDataStore.datastoreServer.commands;
 import java.io.IOException;
 
 import utd.persistentDataStore.utils.ServerException;
+import utd.persistentDataStore.utils.*;
 
 public class ReadCommand extends ServerCommand{
 
 	@Override
 	public void run() throws IOException, ServerException {
-		// TODO Auto-generated method stub
+		String name = "";
+		byte[] data = null;
+		try {
+			name = StreamUtil.readLine(super.inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ServerException("Error reading input.");
+		}
+		
+		try {
+			data = FileUtil.readData(name);
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new ServerException("Cannot locate data.");
+		}
+		
+		super.sendOK();
+		StreamUtil.writeLine(String.valueOf(data.length) + "\n", outputStream);
+		StreamUtil.writeData(data, outputStream);
+		
 		
 	}
 
